c269ee339d7c70bfd6c918e65c9a8939954b966e
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-15 22:15:42 -0500

    fixing the last 2 problems of the _printf.c file

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	va_list parameters;
 
 	va_start(parameters, format);
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
