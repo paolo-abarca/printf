224498e49ed6134de51c09b51f6b5ec823aa9f03
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-15 20:01:25 -0500

    updating _printf.c function for task 0

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, j, string_value = 0;
+	int i, j, string_value = 0, letter_fail = 0;
 	mod_t mod[] = {{"c", char_mod}, {"s", string_mod}, {"d", number_mod},
 			{"i", number_mod}, {NULL, NULL}};
 	va_list parameters;
@@ -25,9 +25,12 @@ int _printf(const char *format, ...)
 				if (format[i + 1] == mod[j].mod[0])
 				{
 					string_value = string_value + mod[j].p(parameters);
+					letter_fail = letter_fail + 1;
 					i++;
-				}
-			}
+				}}
+			if (letter == 0)
+				_putchar(format[i]);
+				string_value = string_value + 1;
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
 		{
@@ -39,8 +42,7 @@ int _printf(const char *format, ...)
 		{
 			_putchar(format[i]);
 			string_value = string_value + 1;
-		}
-	}
+		}}
 	va_end(parameters);
 	return (string_value);
 }
