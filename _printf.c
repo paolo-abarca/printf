3de007948c5b301afce21c9414ad6f502e07d82a
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-03-15 19:11:16 -0500

    fixing _printf.c function for task 0

--- _printf.c
+++ _printf.c
@@ -1,27 +1,21 @@
 #include "main.h"
-
 /**
  * _printf - My own printf function
- *
  * @format: argument pointer 0
  * Return: returning the length of each string
  */
-
 int _printf(const char *format, ...)
 {
-	int i, j;
-	int string_value = 0;
-	mod_t mod[] = {{"c", char_mod},
-			{"s", string_mod},
-			{"d", number_mod},
+	int i, j, string_value = 0;
+	mod_t mod[] = {{"c", char_mod}, {"s", string_mod}, {"d", number_mod},
 			{"i", number_mod}, {NULL, NULL}};
-
 	va_list parameters;
 
 	va_start(parameters, format);
-
 	if (format == NULL)
 		return (-1);
+	if (*format == '\0')
+		return (0);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '%')
@@ -39,12 +33,12 @@ int _printf(const char *format, ...)
 		{
 			_putchar('%');
 			string_value = string_value + 1;
+			i++;
 		}
 		else
 		{
 			_putchar(format[i]);
 			string_value = string_value + 1;
-			i++;
 		}
 	}
 	va_end(parameters);
